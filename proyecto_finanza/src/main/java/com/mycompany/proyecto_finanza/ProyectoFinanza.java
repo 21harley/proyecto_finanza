d63b14865c5b1c769e1696aebbeb31227e4643e9
21harley <john.llanes.1996@gmail.com>
2024-05-19 23:55:34 -0400

    update

---
+++ proyecto_finanza/src/main/java/com/mycompany/proyecto_finanza/ProyectoFinanza.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyecto_finanza;
+
+/**
+ *
+ * @author Uelectronica
+ */
+public class ProyectoFinanza {
+
+    public static void main(String[] args) {
+        System.out.println("Proyecto finanza");
+        System.out.println("Proyecto Hola");
+        System.out.println("Proyecto Hola");
+        System.out.println("Proyecto Hola");
+        Saludo.Hola();
+        Saludo.Hola();
+    }
+
+}
